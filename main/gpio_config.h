8383a8d4b7fad231c978a66711415dbbb82f8be5
David Florian <david.florian2103@gmail.com>
2025-07-31 23:38:25 +0300

    gpio_config.h

--- main/gpio_config.h
+++ main/gpio_config.h
@@ -2,7 +2,25 @@
 #define GPIO_CONFIG_H
 
 #include "driver/gpio.h"
-
+/**
+ * @brief Configure OUTPUT PIN for LED 
+ * 
+* @param gpio_num GPIO PIN_NUM
+ */
 void configure_LED_output(int gpio_num);
+/**
+ * @brief Config INPUT PIN for BUTTON
+ * 
+* @param gpio_num GPIO PIN_NUM
+ */
+void configure_BUTTON_input(int gpio_num);
 
+/**
+ * @brief Config user GPIO PIN
+ * 
+ *
+ *
+ *
+ *
+ */
 #endif // GPIO_CONFIG_H
